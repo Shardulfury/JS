b1e3e9b01bad0fd434496c450ca91262045b35a4
SHARDUL GHUSAR <115490133+Shardulfury@users.noreply.github.com>
2026-06-28 17:08:31 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treate all code as newer version 
+
+// alert(3+3) we are using node js not brower
+
+let name = "shardul"
+let age = 21
+let isLoggesIn = true
+
+// number => 2 to the power 53
+// bigint 
+// string => ""
+// boolean => true false
+//null standalone value
+//undefined
+//symbol => unique
+
+//object 
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
